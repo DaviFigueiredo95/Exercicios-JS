6c94bd60f99440e3ec3fed60150fbf7b15dc0a89
Davi Figueiredo <davifigueiredo0195@gmail.com>
2022-11-29 23:09:20 -0300

    adaptações

--- mediaNotaEscolar.js
+++ mediaNotaEscolar.js
@@ -1,25 +1,24 @@
 // Média escolar com notas armazenadas em array
 
-const array = [10, 100, 80]
+const array = [42, 100, 80];
 
 function mediaDoAluno(notas) {
-    const media = calcularMedia(notas)
+  const media = calcularMedia(notas);
 
-    if(media < 59) return 'Nota F'
-    if(media < 69) return 'Nota D'
-    if(media < 79) return 'Nota C'
-    if(media < 89) return 'Nota B'
-    return 'A'
-
+  if (media < 59) return "Nota F";
+  if (media < 69) return "Nota D";
+  if (media < 79) return "Nota C";
+  if (media < 89) return "Nota B";
+  return "A";
 }
 
 function calcularMedia(array) {
-    let soma = 0
-    for(let valor of array) {
-        soma += valor
-    }
-    let somaFinal = soma / (array.length)
-    return somaFinal
+  let soma = 0;
+  for (let valor of array) {
+    soma += valor;
+  }
+  let somaFinal = soma / Math.floor(array.length);
+  return somaFinal;
 }
 
-console.log(mediaDoAluno(array))
+console.log(mediaDoAluno(array));
